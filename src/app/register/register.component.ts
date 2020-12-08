9813294ccba85f06b45f5429144b0df142f21321
etobaga <64399794+etobaga@users.noreply.github.com>
2020-12-09 02:25:56 +0600

    1

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -21,12 +21,12 @@ export class RegisterComponent implements ComponentCanDeactivate {
   saved = false;
 
   onSubmit(){
-    console.log(this.login.firstname, this.login.lastname, this.login.email, this.login.password, this.login.passwordConfirmation);
+    console.log(this.login.firstname, this.login.lastname, this.login.email, this.login.password, this.login.passwordConfirmation, this.login.city);
     this.saved = true;
   }
   canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
     if ((this.login.firstname.length > 0 || this.login.lastname.length > 0 || this.login.email.length > 0 || this.login.password.length > 0 ||
-      this.login.passwordConfirmation.length > 0)
+      this.login.passwordConfirmation.length > 0 || this.login.city.length > 0)
       && !this.saved) {
       return confirm('Are you sure about it?');
     }
